384204e76a684b18467019948b5c0b12a7fcd8c1
Matej M <mmmeciar@lacoinc.com>
2020-12-04 13:33:57 -0800

    feat(player-class): added boiler plate for player class

---
+++ src/Player.java
@@ -0,0 +1,15 @@
+public class Player {
+    private char symbol;
+
+    public Player(char c) {
+        this.symbol = c;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    };
+
+    public void setSymbol(char c) {
+        this.symbol = c;
+    };
+}
