30aa2fcc73e4fc55873a8a172125ce300a954609
Matej M <mmmeciar@lacoinc.com>
2020-12-04 20:53:14 -0800

    feat(input-provider): created static input provider to provid inputs

---
+++ src/InputProvider.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class InputProvider {
+
+    private static Scanner userIn = new Scanner(System.in);
+
+    public static int getNumberInput(String prompt) {
+        return getNumberInput(prompt, false);
+    }
+
+    private static int getNumberInput(String prompt, boolean nan) {
+        try {
+            if (nan) {
+                System.out.println("\nPlease enter a valid number.");
+            }
+            System.out.print(prompt);
+            return Integer.parseInt(userIn.next());
+        } catch (NumberFormatException e) {
+            getNumberInput(prompt, true);
+        }
+        return 0;
+    }
+
+    public static char getCharInput(String prompt) {
+        return getCharInput(prompt, false);
+    }
+
+    private static char getCharInput(String prompt, boolean invalidResponse) {
+        if (invalidResponse) {
+            System.out.println("\nPlease enter a single character.");
+        }
+        System.out.println(prompt);
+        String input = userIn.next();
+        if (input.length() != 1) {
+            return getCharInput(prompt, true);
+        } else {
+            return input.charAt(0);
+        }
+    }
+}
--- src/MainDriver.java
+++ src/MainDriver.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class MainDriver {
     public static void main(String[] args) {
-        GameBoard gameBoard = new GameBoard();
+        restartGame();
+        // Player test1 = new Player('X');
+        // Player test2 = new Player('O');
+        // gameBoard.makeMove(0, test1);
+        // gameBoard.makeMove(1, test2);
+        // gameBoard.makeMove(8, test1);
+        // gameBoard.makeMove(4, test1);
+        // gameBoard.printBoard();
     }
+
+    private static void restartGame() {
+        int input = 0;
+        while (input != 1 && input != 2) {
+            input = InputProvider.getNumberInput("Do you want to play single player (1) or multiplayer (2)? ");
+        }
+        if (input == 1) {
+            playSinglePlayer();
+        } else {
+            playMultiPlayer();
+        }
+    }
+
 }
